cad2fdb170251d02e3125f88625f25bd4ee486a7
Alan Viana <alanlviana@hotmail.com>
2021-04-24 10:56:37 -0300

    Adicionando CORS

--- Products.API/Startup.cs
+++ Products.API/Startup.cs
@@ -31,6 +31,7 @@ namespace Products.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -99,6 +100,12 @@ namespace Products.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DatabaseContext context)
         {
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+
             app.UseAuthentication();
             
             if (env.IsDevelopment())
